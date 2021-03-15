457e766e3ec645a8a08d6e03f02b12d4a8ed6de7
muratmaga <muratmaga@outlook.com>
2021-03-15 16:09:27 -0700

    added LM Label

--- read.markups.fcsv.R
+++ read.markups.fcsv.R
@@ -1,5 +1,7 @@
 read.markups.fcsv = function (file=NULL) {
   temp = read.csv(file=file, skip = 2, header = T)
-  return ( as.matrix (temp[, 2:4] ) )
+  LM = array (data=as.matrix( temp[,2:4]),
+              dim =c(nrow(temp), 3),
+              dimnames=list(temp$label, c("X", "Y", "Z")))
+  return(LM)
 }
-
