35b0962441da363b0078653ab72b79fb4c824532
muratmaga <muratmaga@outlook.com>
2021-03-15 16:03:20 -0700

    added LM labels

--- read.markups.json.R
+++ read.markups.json.R
@@ -1,12 +1,13 @@
-read.markups.json = function(file=NULL){
+function(file=NULL){
   if (!require(jsonlite)) {
-        print("installing jsonlite")
-        install.packages('jsonlite')
-        library(jsonlite)
+    print("installing jsonlite")
+    install.packages('jsonlite')
+    library(jsonlite)
   }
   dat = fromJSON(file, flatten=T)
   n=length(dat$markups$controlPoints[[1]]$position)
-  temp = array(dim = c(n, 3))
+  labels = dat$markups$controlPoints[[1]]$label
+  temp = array(dim = c(n, 3), dimnames=list(labels, c("X", "Y", "Z")))
   for (i in 1:n) temp[i,] = dat$markups$controlPoints[[1]]$position[[i]]
   return(temp)
 }
