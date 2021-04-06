cd234c673842e27467fffe801952650cb3d51a48
muratmaga <muratmaga@outlook.com>
2021-04-05 21:27:31 -0700

    update for new format change.

--- log_parser.R
+++ log_parser.R
@@ -8,40 +8,42 @@ parser = function(file=NULL){
   log$input.path = temp[3]
   log$output.path = temp[4]
   log$files = unlist(strsplit(temp[5],','))
-  log$format = temp[6]
+  #log$format =  substr(temp[6], start=2, stop = nchar(temp[6]))
+  log$format =  temp[6]
   log$no.LM = as.numeric(temp[7])
+  
+  # checking if any LMs are skipped
   if (is.na(temp[8])) log$skipped=FALSE else {
     log$skipped=TRUE
     log$skipped.LM=unlist(strsplit(temp[8],","))
   }
   
-# parsing if any landmarks are tagged as semiLMs 
+  # parsing if any landmarks are tagged as semiLMs 
   
   if (is.na(temp[15])) log$semi=FALSE else {
     log$semi=TRUE
     log$semiLMs=unlist(strsplit(temp[15],","))
   }
   
-    
+  # checking if scaling to centroid size is disabled during GPA
   if (as.logical(temp[9])) log$scale=TRUE else log$scale=FALSE
+  
   log$MeanShape=temp[10]
   log$eigenvalues=temp[11]
   log$eigenvectors=temp[12]
   log$OutputData=temp[13]
   log$pcScores=temp[14]
-  log$ID=gsub(pattern = paste0(".", log$format), "", fixed=T, log$files )
+  log$ID=gsub(log$format, "", fixed=T, log$files )
   
   if (!log$skipped) {
     log$LM = array(dim =c (log$no.LM, 3, length(log$files)), 
                    dimnames = list(1:log$no.LM, c("x", "y", "z"), log$ID))
-    if (log$format!="json") 
-      for (i in 1:length(log$files)) log$LM[,,i] = read.markups.fcsv(paste(log$input.path,log$files[i],sep = "/")) else 
+    if (log$format==".fcsv") for (i in 1:length(log$files)) log$LM[,,i] = read.markups.fcsv(paste(log$input.path,log$files[i],sep = "/")) else 
         for (i in 1:length(log$files))  log$LM[,,i] = read.markups.json(paste(log$input.path,log$files[i],sep = "/"))
   } else {
     log$LM = array(dim = c(log$no.LM - length(log$skipped.LM), 3, length(log$files)), 
                    dimnames = list((1:log$no.LM)[-as.numeric(log$skipped.LM)], c("x", "y", "z"), log$ID))
-    if (log$format!="json") 
-      for (i in 1:length(log$files)) log$LM[,,i] = read.markups.fcsv(paste(log$input.path,log$files[i],sep = "/"))[-c(as.numeric(log$skipped.LM) ), ] else 
+    if (log$format==".fcsv") for (i in 1:length(log$files)) log$LM[,,i] = read.markups.fcsv(paste(log$input.path,log$files[i],sep = "/"))[-c(as.numeric(log$skipped.LM) ), ] else 
         for (i in 1:length(log$files)) log$LM[,,i] = read.markups.json(paste(log$input.path,log$files[i],sep = "/"))[-c(as.numeric(log$skipped.LM) ), ]  
         
   }
